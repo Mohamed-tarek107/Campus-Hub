5125a0d18f2acb93ec59f1b364bc98a0a732b4f8
Mohamed <mohamedtarekk710@gmail.com>
2026-02-20 22:49:20 +0200

    Fixed db connection instead of connection string using credntials from .env + fixed mismatch name from server.js and .env

--- backend/server.js
+++ backend/server.js
@@ -49,5 +49,5 @@ app.use((err, req, res, next) => {
     res.status(500).json({ message: "Something went wrong" });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.SERVER_PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
