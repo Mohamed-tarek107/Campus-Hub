095ab03debb2b9cb1fea9c8f04dcb901056cd89e
Mohamed <mohamedtarekk710@gmail.com>
2026-02-21 22:23:45 +0200

    Adapted route login as default

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { LoginPage } from './components/login-page/login-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', redirectTo: 'Login', pathMatch: 'full'},
+    { path: 'Login', component: LoginPage },
+];
