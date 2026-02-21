ac84cfbaca524e4d7d958d7dbd183a8c1da63b8b
Mohamed <mohamedtarekk710@gmail.com>
2026-02-21 14:22:26 +0200

    Enviroment Control best practices

--- backend/server.js
+++ backend/server.js
@@ -10,19 +10,10 @@ app.use(helmet());
 app.use(cookieParser());
 app.use(express.json());
 
-const allowedOrigins = process.env.ALLOWED_ORIGINS
-    ? process.env.ALLOWED_ORIGINS.split(',')
-    : ["http://localhost:4200"];
+const isProd = process.env.NODE_ENV === "production"
 
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    },
+    origin: isProd ? process.env.FRONTEND_PROD_URL: process.env.FRONTEND_URL ,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"]
