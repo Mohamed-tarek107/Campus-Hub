0d610dc3f3fb5f55c40739f2a91e621c38f0d419
Mohamed <mohamedtarekk710@gmail.com>
2026-03-02 05:56:25 +0200

    Minor changes in navs

--- frontend/src/app/components/sidenav/sidenav.component.ts
+++ frontend/src/app/components/sidenav/sidenav.component.ts
@@ -1,12 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidenav',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, CommonModule],
   templateUrl: './sidenav.component.html',
   styleUrl: './sidenav.component.css',
 })
 export class SidenavComponent {
+  isCollapsed = true;
 
+  toggle(): void {
+    this.isCollapsed = !this.isCollapsed;
+  }
 }
