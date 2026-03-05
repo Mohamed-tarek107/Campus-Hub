d879512b153195e421528985225a3b38d54b6407
Mohamed <mohamedtarekk710@gmail.com>
2026-03-05 05:47:08 +0200

    Created components for admin routes sidenav and topnav(just created)

---
+++ frontend/src/app/components/Admin/admin.topnav/admin.topnav.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin.topnav.component',
+  imports: [],
+  templateUrl: './admin.topnav.component.html',
+  styleUrl: './admin.topnav.component.css',
+})
+export class AdminTopnavComponent {
+
+}
