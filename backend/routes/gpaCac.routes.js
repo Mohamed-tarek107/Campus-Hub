27fb5781de00a354d872af806bf2fc3f907d0850
Mohamed <mohamedtarekk710@gmail.com>
2026-03-05 09:19:39 +0200

    Routed aichat bot route

--- backend/routes/gpaCac.routes.js
+++ backend/routes/gpaCac.routes.js
@@ -1,9 +1,9 @@
 const express = require("express");
-const { editUserGpa } = require("../controllers/AI_gpaCalc.js");
+const { editUserGpa, chatBot } = require("../controllers/AI_gpaCalc.js");
 const { ensureAuthenticated } = require("../middlewares/authMiddleware.js")
 const router = express.Router();
 
 
 router.post("/assginGpa", ensureAuthenticated, editUserGpa)
-
+router.post("/aiChat", ensureAuthenticated, chatBot)
 module.exports = router
