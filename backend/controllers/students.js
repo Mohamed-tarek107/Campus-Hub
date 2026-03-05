1921dacbf5f31b61bd3323dd9d19d14174cb1df2
Mohamed <mohamedtarekk710@gmail.com>
2026-03-05 08:56:53 +0200

    Created takefeedback route for user

--- backend/controllers/students.js
+++ backend/controllers/students.js
@@ -167,4 +167,22 @@ const viewAllstudent_tasks = async (req, res) => {
     }
 };
 
-module.exports = { AssignDoctors, viewAllstudent_courses, viewAllstudent_doctors, viewAllstudent_tasks, getCourseDoctors, getStudentCourses};
+
+const takeFeedback = async (req,res) => {
+    const user_id = req.user.id;
+    try {
+        const { feedback } = req.body
+
+        if(!feedback) return res.status(404).json({ message: "Please Provide feedback" })
+
+        await db.query("INSERT INTO feedbacks (user_id, feedback) VALUES ($1,$2)", [user_id, feedback])
+
+        return res.status(200).json({ message: "Feedback Taken Successfully" });
+    } catch (error) {
+        console.error("viewAllStudent_tasks error:", error.message);
+        return res.status(500).json({ message: "Server error" });
+    }
+}
+
+
+module.exports = { AssignDoctors, viewAllstudent_courses, viewAllstudent_doctors, viewAllstudent_tasks, getCourseDoctors, getStudentCourses, takeFeedback};
