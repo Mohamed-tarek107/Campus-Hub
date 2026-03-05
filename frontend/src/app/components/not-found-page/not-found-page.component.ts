61819643acf88313052f965567ad79c56b10c442
Mohamed <mohamedtarekk710@gmail.com>
2026-03-05 05:39:18 +0200

    Created a notfoundpage component for routing in a wrong route

---
+++ frontend/src/app/components/not-found-page/not-found-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found-page.component',
+  imports: [RouterModule],
+  templateUrl: './not-found-page.component.html',
+  styleUrl: './not-found-page.component.css',
+})
+export class NotFoundPageComponent {}
