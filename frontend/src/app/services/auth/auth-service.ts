255cee5f83aa616b29281b3d87a2e3cf26b84d7c
Mohamed <mohamedtarekk710@gmail.com>
2026-03-08 11:59:22 +0200

    Started with auth service and made register route using httpclient

--- frontend/src/app/services/auth/auth-service.ts
+++ frontend/src/app/services/auth/auth-service.ts
@@ -1,11 +1,36 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../enviroments/envoriment';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
-    userInfo() {
-        throw new Error('Method not implemented.');
+    private readonly authApi = `${environment.apiUrl}/auth`
+    isLoggedin = false
+
+    constructor(private http: HttpClient){}
+
+
+    register(
+        username: string, 
+        email: string,
+        password: string,
+        confirmpassword: string,
+        department: string,
+        year: number
+    ){
+        return this.http.post(`${this.authApi}/register`, {
+            username,
+            email,
+            password, 
+            confirmpassword,
+            department,
+            year
+        })
     }
-  
+    
+    
+
+
 }
