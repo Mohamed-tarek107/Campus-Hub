8db8709d55ab315c0f6b1f5a3d169bb227fe6bee
Mohamed <mohamedtarekk710@gmail.com>
2026-03-13 15:01:28 +0200

    Completed the gpaCalc service with 2 functions to assign gpa and chatbot with msg and history for gemini ai

--- frontend/src/app/services/gpaCalc/gpa-calc-service.ts
+++ frontend/src/app/services/gpaCalc/gpa-calc-service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../../../enviroments/envoriment';
+import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class GpaCalcService {
-  
+  private readonly gpaApi = `${environment.apiUrl}/gpa`
+
+  constructor(private http: HttpClient){}
+
+  editUserGpa(gpa: Number){
+    return this.http.post(`${this.gpaApi}/assginGpa`,
+      { gpa },
+      {withCredentials: true})
+  }
+
+  chatBot(msg: string, history: []){
+    return this.http.post(`${this.gpaApi}/aiChat`,
+    { msg, history },
+    {withCredentials: true})
+  }
 }
