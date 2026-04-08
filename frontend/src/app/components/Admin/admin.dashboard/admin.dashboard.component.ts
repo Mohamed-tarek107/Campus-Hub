9bf42f55b92a888542f4214c777663058e8f4c59
Mohamed <mohamedtarekk710@gmail.com>
2026-04-08 17:33:10 +0200

    Finished admin dashboard with one api call and detectchanges

--- frontend/src/app/components/Admin/admin.dashboard/admin.dashboard.component.ts
+++ frontend/src/app/components/Admin/admin.dashboard/admin.dashboard.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AdminSidenavComponent } from '../admin.sidenav/admin.sidenav.component';
 import { AdminTopnavComponent } from '../admin.topnav/admin.topnav.component';
+import { AdminPanelService } from '../../../services/admin/admin-panel';
 
 @Component({
   selector: 'app-admin.dashboard.component',
+  standalone: true,
   imports: [CommonModule, RouterModule, AdminSidenavComponent, AdminTopnavComponent],
   templateUrl: './admin.dashboard.component.html',
   styleUrl: './admin.dashboard.component.css',
@@ -14,9 +16,22 @@ export class AdminDashboardComponent implements OnInit {
 
   totalStudents = 0;
   totalDoctors  = 0;
+  totalCourses = 0;
+
+  constructor(private cdr: ChangeDetectorRef, private adminServie: AdminPanelService){}
 
   ngOnInit(): void {
     // TODO: GET /api/admin/stats → set totalStudents, totalDoctors
+    this.adminServie.dashboardStats().subscribe({
+      next: (data: any) => {
+        this.totalCourses = data.courses_count
+        this.totalDoctors = data.doctors_count
+        this.totalStudents = data.students_count
+        this.cdr.detectChanges();
+      }, 
+      error: (err) => {
+        console.error(err.message ? err.message : err);
+      }
+    })
   }
-
 }
